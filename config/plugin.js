81bc84ed5fa0ac89ed6ee253cf40f90ea0cdf506
shiyici <13162688027@163.com>
2022-07-24 07:53:07 +0800

    init repository

---
+++ config/plugin.js
@@ -0,0 +1,14 @@
+'use strict'
+
+exports.nunjucks = {
+  enable: true,
+  package: 'egg-view-nunjucks',
+}
+
+/** @type Egg.EggPlugin */
+module.exports = {
+  // had enabled by egg
+  // static: {
+  //   enable: true,
+  // }
+}
